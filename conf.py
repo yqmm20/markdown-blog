ca5549708273c49d96007ba7e9d279f700e4acc6
yqmm20 <59204535+yqmm20@users.noreply.github.com>
2019-12-24 23:25:44 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,10 +14,10 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "markdown-blog"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
+author = "tu-tu"
 email = "hi@imalan.cn"
 author_homepage = "https://www.imalan.cn"
 description = "只坚持一种正义。我的正义。"
