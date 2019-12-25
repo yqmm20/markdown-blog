cb555f329070bbede74f061f441caf00f61edf20
yqmm20 <p5u6k@boun.cr>
2019-12-25 08:48:50 +0800

    Update my site

--- update_maverick.sh
+++
@@ -1,9 +0,0 @@
-#！/bin/sh
-
-cd Maverick
-git pull --rebase
-
-cd ..
-git add .
-git commit -m "Update Maverick"
-git push
--- update_site.sh
+++
@@ -1,5 +0,0 @@
-#！/bin/sh
-
-git add .
-git commit -m "Update my site"
-git push
